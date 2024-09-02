039790b14342460a381e41e6881834cce6572fcc
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-09-02 01:15:32 -0600

    Update render_mm_animations.jl

--- mm_animation/render_mm_animations.jl
+++ mm_animation/render_mm_animations.jl
@@ -11,8 +11,8 @@ function mm_curve(vmax, km)
     mm.(substrate_concentrations)
 end
 
-vmax = 1.0e-3
-kms = range(start=2.5e-4, stop=1.0e-3, length=150)
+vmax = 2.0e-3
+kms = range(start=1.0e-3, stop=1.0e-2, length=150)
 
 anim = Animation()
 
@@ -20,12 +20,13 @@ for (index, km) in enumerate(kms)
     ys = mm_curve(vmax, km)
 
     xtick_vals = range(start=0, stop=maximum(substrate_concentrations), length=5)
-    xtick_labels = [@sprintf("%.3e", val) for val in xtick_vals]
+    xtick_labels = [@sprintf("%.1e", val) for val in xtick_vals]
 
     ytick_vals = collect(range(start=0, stop=vmax, length=5))
-    ytick_labels = [@sprintf("%.3e", val) for val in ytick_vals]
+    ytick_labels = [@sprintf("%.1e", val) for val in ytick_vals]
 
-    label = "Km=$km"
+    km_formatted = @sprintf("%.3e", km)
+    label = "Km=$km_formatted"
 
     plot(
         substrate_concentrations, 
@@ -37,7 +38,8 @@ for (index, km) in enumerate(kms)
         ylabel="d[P]/dt (M/min)", 
         xticks=(xtick_vals, xtick_labels), 
         yticks=(ytick_vals, ytick_labels), 
-        linewidth=3
+        linewidth=3,
+        legend=:right
     )
 
     frame(anim)
