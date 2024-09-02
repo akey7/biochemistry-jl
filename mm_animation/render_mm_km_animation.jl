1707a5c1b49eead05b43d1aaf9dc4bec1ffc331d
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-09-02 10:16:44 -0600

    Put Km animation into its own function.

--- mm_animation/render_mm_km_animation.jl
+++ mm_animation/render_mm_km_animation.jl
@@ -15,61 +15,66 @@ function mm_curve(vmax, km)
     mm.(substrate_concentrations)
 end
 
-# vmax and km range for curves
-vmax = 2.0e-3
-start_km = 1.0e-2
-stop_km = 1.0e-3
-kms = range(start=start_km, stop=stop_km, length=150)
+# Creates the Km animation
+function render_km_animation()
+    # vmax and km range for curves
+    vmax = 2.0e-3
+    start_km = 1.0e-2
+    stop_km = 1.0e-3
+    kms = range(start=start_km, stop=stop_km, length=150)
 
-# Create an animation object
-anim = Animation()
+    # Create an animation object
+    anim = Animation()
 
-# Iterate over every km in the sequence, hold vmax constant, and
-# render a frame for each km. Format floats to appropriate number
-# of decimal places. Give status update every 10 frames.
+    # Iterate over every km in the sequence, hold vmax constant, and
+    # render a frame for each km. Format floats to appropriate number
+    # of decimal places. Give status update every 10 frames.
 
-for (index, km) in enumerate(kms)
-    ys = mm_curve(vmax, km)
+    for (index, km) in enumerate(kms)
+        ys = mm_curve(vmax, km)
 
-    xtick_vals = range(start=0, stop=maximum(substrate_concentrations), length=5)
-    xtick_labels = [@sprintf("%.1e", val) for val in xtick_vals]
+        xtick_vals = range(start=0, stop=maximum(substrate_concentrations), length=5)
+        xtick_labels = [@sprintf("%.1e", val) for val in xtick_vals]
 
-    ytick_vals = collect(range(start=0, stop=vmax, length=5))
-    ytick_labels = [@sprintf("%.1e", val) for val in ytick_vals]
+        ytick_vals = collect(range(start=0, stop=vmax, length=5))
+        ytick_labels = [@sprintf("%.1e", val) for val in ytick_vals]
 
-    vmax_formatted = @sprintf("%.2e", vmax)
-    km_formatted = @sprintf("%.2e", km)
-    title = "Vmax=$vmax_formatted"
+        vmax_formatted = @sprintf("%.2e", vmax)
+        km_formatted = @sprintf("%.2e", km)
+        title = "Vmax=$vmax_formatted"
 
-    annotation_x = km + maximum(substrate_concentrations) * 0.175
-    annotation_y = 0.95 * vmax
+        annotation_x = km + maximum(substrate_concentrations) * 0.175
+        annotation_y = 0.95 * vmax
 
-    plot(
-        substrate_concentrations, 
-        ys, 
-        title=title,
-        xlims=(0.0, maximum(substrate_concentrations) * 1.1),
-        ylims=(0.0, vmax * 1.01),
-        xlabel="[S] (M)", 
-        ylabel="d[P]/dt (M/min)", 
-        xticks=(xtick_vals, xtick_labels), 
-        yticks=(ytick_vals, ytick_labels), 
-        linewidth=3,
-        legend=:none
-    )
+        plot(
+            substrate_concentrations, 
+            ys, 
+            title=title,
+            xlims=(0.0, maximum(substrate_concentrations) * 1.1),
+            ylims=(0.0, vmax * 1.01),
+            xlabel="[S] (M)", 
+            ylabel="d[P]/dt (M/min)", 
+            xticks=(xtick_vals, xtick_labels), 
+            yticks=(ytick_vals, ytick_labels), 
+            linewidth=3,
+            legend=:none
+        )
 
-    vline!([km], label="Km", linecolor=:orange, linestyle=:dot, linewidth=3)
-    annotate!(annotation_x, annotation_y, text("Km=$km_formatted", :orange, 12))
+        vline!([km], label="Km", linecolor=:orange, linestyle=:dot, linewidth=3)
+        annotate!(annotation_x, annotation_y, text("Km=$km_formatted", :orange, 12))
 
-    frame(anim)
+        frame(anim)
 
-    if index % 10 == 0
-        println("Frame $index rendered...")
+        if index % 10 == 0
+            println("Frame $index rendered...")
+        end
     end
+
+    # Write the animation to a file
+    mp4(anim, "mm_km_animation.mp4", fps=30)
+
+    # Give the final status update
+    println("Wrote Km animation!")
 end
 
-# Write the animation to a file
-mp4(anim, "mm_km_animation.mp4", fps=30)
-
-# Give the final status update
-println("Done!")
+render_km_animation()
