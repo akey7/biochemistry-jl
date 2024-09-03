7f92e65ee88efd59b2a3b43a4290ea9addb3fa5e
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-09-03 15:39:52 -0600

    Update render_linear_pathway.jl

--- linear_pathway_animation/render_linear_pathway.jl
+++ linear_pathway_animation/render_linear_pathway.jl
@@ -7,13 +7,17 @@ steps = 1000
 h = duration_minutes / steps
 
 #####################################################################
-# METABOLITE CONCENTRATION VALUES AT EACH STEP                      #
+# FUNCTION TO INTITIALIZE INPUT VECTORS                             #
 #####################################################################
 
-x1 = zeros(Float64, steps)
-x2 = zeros(Float64, steps)
-x3 = zeros(Float64, steps)
-x4 = zeros(Float64, steps)
-x5 = zeros(Float64, steps)
-
-
+function intitialize_inputs(input1_level=0.1, input_2_on_level=0.99, input_2_off_level=0.01, input_2_turn_off_step=101, input_2_turn_on_step=601)
+    inputs = zeros(Float64, 2, steps)
+    inputs[1, :] = input1_level
+    for j in 1:length(inputs[2, :])
+        if j <= input_2_turn_on_step && j >= input_2_turn_off_step
+            inputs[2, j] = input_2_on_level
+        else
+            inputs[2, j] = input_2_off_level
+        end
+    end
+end
