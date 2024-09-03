bd35019c23f088a7354cb78f1a0e7c83c97db2a3
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-09-03 15:26:20 -0600

    Update render_linear_pathway.jl

---
+++ linear_pathway_animation/render_linear_pathway.jl
@@ -0,0 +1,19 @@
+#####################################################################
+# LENGTH AND NUMBER OF STEPS IN SIMULATION                          #
+#####################################################################
+
+duration_minutes = 100
+steps = 1000
+h = duration_minutes / steps
+
+#####################################################################
+# METABOLITE CONCENTRATION VALUES AT EACH STEP                      #
+#####################################################################
+
+x1 = zeros(Float64, steps)
+x2 = zeros(Float64, steps)
+x3 = zeros(Float64, steps)
+x4 = zeros(Float64, steps)
+x5 = zeros(Float64, steps)
+
+
