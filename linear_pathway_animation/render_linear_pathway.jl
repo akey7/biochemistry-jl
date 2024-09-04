48796cde6f1fa5eb75fcad0d5ecc5cee653ca27f
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-09-04 07:38:44 -0600

    Update render_linear_pathway.jl

--- linear_pathway_animation/render_linear_pathway.jl
+++ linear_pathway_animation/render_linear_pathway.jl
@@ -99,20 +99,21 @@ xs = range(
 xtick_vals = range(start=minimum(xs), stop=maximum(xs), length=5)
 xtick_labels = [@sprintf("%.1f", val) for val in xtick_vals]
 
-ytick_vals = range(start=0.0, stop=maximum(result[:x]), length=5)
-ytick_labels = [@sprintf("%.1f", val) for val in ytick_vals]
+output_ytick_vals = range(start=0.0, stop=maximum(result[:x]), length=5)
+output_ytick_labels = [@sprintf("%.1f", val) for val in output_ytick_vals]
 
 plot(
     xs, 
     result[:x],
     xticks=(xtick_vals, xtick_labels),
-    yticks=(ytick_vals, ytick_labels),
+    yticks=(output_ytick_vals, output_ytick_labels),
     xlims=(0.0, maximum(xs) * 1.01),
     ylims=(0.0, maximum(result[:x]) * 1.01),
     xlabel="sec",
     ylabel="concentration",
     size=(size_x, size_y)
 )
+
 savefig("Fig 5A.png")
 
 println("Rendering done!")
