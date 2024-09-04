783c94b56d06690d0b0c583c3d0412920726d57b
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-09-03 19:18:09 -0600

    Update render_linear_pathway.jl

--- linear_pathway_animation/render_linear_pathway.jl
+++ linear_pathway_animation/render_linear_pathway.jl
@@ -1,8 +1,6 @@
-#####################################################################
-# LENGTH AND NUMBER OF STEPS IN SIMULATION                          #
-#####################################################################
-
-
+using Plots
+using PyPlot
+pyplot()
 
 #####################################################################
 # FUNCTION TO CALCULATE TRAJECTORY                                  #
@@ -13,22 +11,46 @@ function trajectory(c)
     x = zeros(Float64, c[:steps], 5)
     
     # Setup inputs into the system
-    inputs = zeros(Float64, c[:steps], 2)
+    in = zeros(Float64, c[:steps], 2)
     
-    for i in 1:length(inputs[:, 1])
-        inputs[i, 1] = c[:input1_level]
+    for i in 1:length(in[:, 1])
+        in[i, 1] = c[:input1_level]
     end
 
-    for i in 1:length(inputs[:, 2])
+    for i in 1:length(in[:, 2])
         if i <= c[:input_2_turn_on_step] && i >= c[:input_2_turn_off_step]
-            inputs[i, 2] = c[:input_2_on_level]
+            in[i, 2] = c[:input_2_on_level]
         else
-            inputs[i, 2] = c[:input_2_off_level]
+            in[i, 2] = c[:input_2_off_level]
         end
     end
 
     # Setup initial conditions
     x[1, 1:5] .= c[:initial_conditions]
+
+    # Run the trajectory
+    h = c[:duration_minutes] / c[:steps]
+    h42 = c[:h42]
+    for i in 2:length(x[:, 1])
+        x1 = x[i-1, 1]
+        x2 = x[i-1, 2]
+        x3 = x[i-1, 3]
+        x4 = x[i-1, 4]
+        x5 = x[i-1, 5]
+        in1 = in[i-1, 1]
+        in2 = in[i-1, 2]
+        x[i, 1] = x1 + h*(in1 + in2*x4^0.5 - x1^0.5)
+        x[i, 2] = x2 + h*(x1^0.5 - x2^0.75)
+        x[i, 3] = x3 + h*(2*x2^0.75 - 2*x3^0.4)
+        x[i, 4] = x4 + h*(2*x3^0.4 - in2*x4^0.5 - x2^h42*x4^0.5)
+        x[i, 5] = x5 + h*(x2^h42*x4^0.5 - x5^0.5)
+
+        if i % 10 == 0
+            println("Passed iteration $i")
+        end
+    end
+
+    x 
 end
 
 #####################################################################
@@ -43,7 +65,10 @@ reasonable_defaults = Dict(
     :input_2_off_level => 0.01,
     :input_2_turn_off_step => 101, 
     :input_2_turn_on_step => 601,
+    :h42 => 0.75,
     :initial_conditions => [1.0, 1.0, 1.0, 1.0, 1.0]
 )
 
-trajectory(reasonable_defaults)
+x_trajectory = trajectory(reasonable_defaults)
+scatter(x=1:1000, y=x_trajectory[:, 1])
+savefig("foo.png")
