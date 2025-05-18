0c8740f0a4e6138844c8622d37ac7d6790cd991e
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-18 12:38:46 -0600

    Update interactive_mm.jl

--- mm_interactive/interactive_mm.jl
+++ mm_interactive/interactive_mm.jl
@@ -1,3 +1,8 @@
 using Gtk
 
-
+b_filename = joinpath("mm_interactive", "interactive_mm_ui.glade")
+b = GtkBuilder(filename = b_filename)
+win = b["window_01"]
+showall(win)
+readline()
+println("Press enter to terminate script...")
