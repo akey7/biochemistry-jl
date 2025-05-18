d9b142f6101f6058ac2f3747796ed47bde661723
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-18 12:50:36 -0600

    Enable the slider to be adjusted.

--- mm_interactive/interactive_mm.jl
+++ mm_interactive/interactive_mm.jl
@@ -4,5 +4,5 @@ b_filename = joinpath("mm_interactive", "interactive_mm_ui.glade")
 b = GtkBuilder(filename = b_filename)
 win = b["window_01"]
 showall(win)
+println("Press enter to exit script and close window...")
 readline()
-println("Press enter to terminate script...")
