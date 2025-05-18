5baa56f325867f374009ae5835c9ef5c46101415
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-18 13:03:12 -0600

    Report value of slider

--- mm_interactive/interactive_mm.jl
+++ mm_interactive/interactive_mm.jl
@@ -3,6 +3,15 @@ using Gtk
 b_filename = joinpath("mm_interactive", "interactive_mm_ui.glade")
 b = GtkBuilder(filename = b_filename)
 win = b["window_01"]
+scale_01 = b["scale_01"]
+
+function scale_01_value_changed(widget, others...)
+    value = GAccessor.value(scale_01)
+    println("Slider value is $value")
+end
+
+signal_connect(scale_01_value_changed, scale_01, "value-changed")
+
 showall(win)
 println("Press enter to exit script and close window...")
 readline()
