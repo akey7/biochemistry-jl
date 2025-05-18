3425d2590377b6b6b80181888e400fb7806357b9
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-18 13:38:21 -0600

    Enable adjustments to both Km and Vmax

--- mm_interactive/interactive_mm.jl
+++ mm_interactive/interactive_mm.jl
@@ -3,16 +3,27 @@ using Gtk
 b_filename = joinpath("mm_interactive", "interactive_mm_ui.glade")
 b = GtkBuilder(filename = b_filename)
 win = b["window_01"]
-scale_01 = b["scale_01"]
+scale_km = b["scale_km"]
+scale_vmax = b["scale_vmax"]
 
-function scale_01_value_changed(widget, others...)
-    value = GAccessor.value(scale_01)
+function scale_km_value_changed(widget, others...)
+    value = GAccessor.value(scale_km)
+    value *= 1e-4
     Threads.@spawn begin
-        println("Slider value is $value")
+        println("Km = $value")
     end
 end
 
-signal_connect(scale_01_value_changed, scale_01, "value-changed")
+function scale_vmax_value_changed(widget, others...)
+    value = GAccessor.value(scale_vmax)
+    value *= 1e-4
+    Threads.@spawn begin
+        println("Vmax = $value")
+    end
+end 
+
+signal_connect(scale_km_value_changed, scale_km, "value-changed")
+signal_connect(scale_vmax_value_changed, scale_vmax, "value-changed")
 
 showall(win)
 println("Press enter to exit script and close window...")
