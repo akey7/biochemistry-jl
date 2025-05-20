e35301bd38b1650ca280e1420f8c1239423954d4
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-20 07:11:12 -0600

    Rename function for clarity.

--- mm_interactive/interactive_mm.jl
+++ mm_interactive/interactive_mm.jl
@@ -7,13 +7,13 @@ gr()
 
 substrate = range(1.0e-6, 1.0e-2, 10)
 
-function v_curve(km, vmax, inhibitor, ki)
+function competitive_inhibition_curve(km, vmax, inhibitor, ki)
     num = vmax .* substrate
     denom = substrate .+ km * (1 + inhibitor/ki)
     return num ./ denom
 end
 
-function plot_v_curve(vs)
+function plot_competitive_inhibition_curve(vs)
     plt = plot(substrate, vs)
     buf = IOBuffer()
     Plots.png(plt, buf)
@@ -43,8 +43,8 @@ function button_update_clicked(widget, others...)
     inhibitor *= 1e-4
     ki *= 1e-4
     println("km=$km vmax=$vmax inhibitor=$inhibitor ki=$ki")
-    vs = v_curve(km, vmax, inhibitor, ki)
-    img = plot_v_curve(vs)
+    vs = competitive_inhibition_curve(km, vmax, inhibitor, ki)
+    img = plot_competitive_inhibition_curve(vs)
     ctx = getgc(canvas_01)
     set_source_surface(ctx, img)
     paint(ctx)
