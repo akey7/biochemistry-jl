257ccd139f66d0cdff912c987cb3c58e48cfd53a
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-23 11:50:01 -0600

    Issue #31
    Minimally display the UI.

---
+++ hb_o2_binding/hb_o2.jl
@@ -0,0 +1,23 @@
+using Base64
+using Gtk
+using Plots
+using Cairo
+
+gr()
+
+b_filename = joinpath("hb_o2_binding", "hb_o2_binding_ui.glade")
+b = GtkBuilder(filename = b_filename)
+win = b["window_01"]
+frame_01 = b["frame_01"]
+canvas_01 = GtkCanvas()
+push!(frame_01, canvas_01)
+
+showall(win)
+# button_update_clicked(button_update)  # Get the initial default values from UI
+if !isinteractive()
+    c = Condition()
+    signal_connect(win, :destroy) do widget
+        notify(c)
+    end
+    wait(c)
+end
