eddcbfa59252d959ee2cf4384474da15c84ff6a1
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-23 12:45:18 -0600

    Issue #31
    Calculate the fraction bound curve.

--- hb_o2_binding/hb_o2.jl
+++ hb_o2_binding/hb_o2.jl
@@ -5,6 +5,22 @@ using Cairo
 
 gr()
 
+po2s = range(0.0, 100.0, 10)  # mmHg
+p50_0 = 26.0  # mmHg 
+n = 2.7  # Hill coefficient
+beta = 0.5  # unitless
+gamma = 0.03  # unitless
+delta = 0.15  # unitless
+ph_0 = 7.2  # pH
+co2_0 = 40  # mmHg
+bpg_0 = 5  # mM
+
+p50(ph, co2, bpg) = p50_0 * 10^(beta*(ph-ph_0) + gamma*(co2-co2_0) + delta*(bpg-bpg_0))
+
+function fraction_bound(ph, co2, bpg)
+    @. po2s^n / (p50(ph, co2, bpg)^n + po2s^n)
+end
+
 b_filename = joinpath("hb_o2_binding", "hb_o2_binding_ui.glade")
 b = GtkBuilder(filename = b_filename)
 win = b["window_01"]
@@ -21,6 +37,7 @@ function button_update_clicked(widget, others...)
     co2 = GAccessor.value(scale_co2)
     bpg = GAccessor.value(scale_bpg)
     println("ph=$ph co2=$co2 bpg=$bpg")
+    println(fraction_bound(ph, co2, bpg))
 end
 
 signal_connect(button_update_clicked, button_update, "clicked")
