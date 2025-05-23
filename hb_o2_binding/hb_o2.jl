0ab238ae75687f3814f3461029810e3cfa414029
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-05-23 12:31:12 -0600

    Update hb_o2.jl
    Add handler for update.

--- hb_o2_binding/hb_o2.jl
+++ hb_o2_binding/hb_o2.jl
@@ -9,11 +9,24 @@ b_filename = joinpath("hb_o2_binding", "hb_o2_binding_ui.glade")
 b = GtkBuilder(filename = b_filename)
 win = b["window_01"]
 frame_01 = b["frame_01"]
+scale_ph = b["scale_ph"]
+scale_co2 = b["scale_co2"]
+scale_bpg = b["scale_bpg"]
+button_update = b["button_update"]
 canvas_01 = GtkCanvas()
 push!(frame_01, canvas_01)
 
+function button_update_clicked(widget, others...)
+    ph = GAccessor.value(scale_ph)
+    co2 = GAccessor.value(scale_co2)
+    bpg = GAccessor.value(scale_bpg)
+    println("ph=$ph co2=$co2 bpg=$bpg")
+end
+
+signal_connect(button_update_clicked, button_update, "clicked")
+
 showall(win)
-# button_update_clicked(button_update)  # Get the initial default values from UI
+button_update_clicked(button_update)  # Get the initial default values from UI
 if !isinteractive()
     c = Condition()
     signal_connect(win, :destroy) do widget
