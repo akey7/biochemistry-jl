b256ee8ba2f5cd642af5b574652a24bb9f94112b
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-11-02 13:13:30 -0700

    Issue #33
    First lines of Repressilator code.

---
+++ repressilator/demo.jl
@@ -0,0 +1,23 @@
+# From Catalyst.jl docs:
+# https://docs.sciml.ai/Catalyst/stable/introduction_to_catalyst/introduction_to_catalyst/
+
+using Catalyst, OrdinaryDiffEqTsit5, Plots
+
+rn = @reaction_network Repressilator begin
+    hillr(P₃,α,K,n), ∅ --> m₁
+    hillr(P₁,α,K,n), ∅ --> m₂
+    hillr(P₂,α,K,n), ∅ --> m₃
+    (δ,γ), m₁ <--> ∅
+    (δ,γ), m₂ <--> ∅
+    (δ,γ), m₃ <--> ∅
+    β, m₁ --> m₁ + P₁
+    β, m₂ --> m₂ + P₂
+    β, m₃ --> m₃ + P₃
+    μ, P₁ --> ∅
+    μ, P₂ --> ∅
+    μ, P₃ --> ∅
+end
+
+println(species(rn))
+println(parameters(rn))
+println(reactions(rn))
