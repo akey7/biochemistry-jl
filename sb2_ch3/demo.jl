93f64567f3d8cfd3d9e8c550ab35cef92d58a422
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-11-09 10:44:51 -0700

    Issue #35
    Bypassing the DSL for total control.

--- sb2_ch3/demo.jl
+++ sb2_ch3/demo.jl
@@ -1,26 +1,26 @@
-using Catalyst, OrdinaryDiffEqTsit5, Plots
-using CairoMakie, GraphMakie, NetworkLayout
+using Catalyst
 
-rn = @reaction_network sb2_ch3_ex begin
-    @species begin
-        x1(t)
-        x2(t)
-        x3(t)
-        x4(t)
-    end
-
-    @parameters begin
-        k_v1_f
-        k_v1_r
-        k_v2_f
-        k_v2_r
-        k_v3_f
-        # k_v3_r is omitted
-    end
-
-    (k_v1_f, k_v1_r), x1 <--> x2
-    (k_v2_f, k_v2_r), x2 <--> x3
-    k_v3_f, x3 --> x4
+t = default_t()
+@species begin
+    x1(t)
+    x2(t)
+    x3(t)
+    x4(t)
 end
-
+@parameters begin
+    k_v1_f
+    k_v1_r
+    k_v2_f
+    k_v2_r
+    k_v3_f
+    # k_v3_r is omitted
+end
+rxs = [
+    Reaction(k_v1_f, [x1], [x2]),
+    Reaction(k_v1_r, [x2], [x1]),
+    Reaction(k_v2_f, [x2], [x3]),
+    Reaction(k_v2_r, [x3], [x2]),
+    Reaction(k_v3_f, [x3], [x4])
+]
+@named rn = ReactionSystem(rxs ,t)
 println(rn.rxs)
