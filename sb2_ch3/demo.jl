e872f49588cc4bdb8b3b9465d11ff47439abf779
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-11-09 11:04:37 -0700

    Issue #35
    Adding rate laws that support PERCs.

--- sb2_ch3/demo.jl
+++ sb2_ch3/demo.jl
@@ -8,19 +8,27 @@ t = default_t()
     x4(t)
 end
 @parameters begin
+    Keq_v1_f
     k_v1_f
+    Keq_v1_r
     k_v1_r
+    Keq_v2_f
     k_v2_f
+    Keq_v2_r
     k_v2_r
+    Keq_v3_f
     k_v3_f
     # k_v3_r is omitted
 end
 rxs = [
-    Reaction(k_v1_f, [x1], [x2]),
-    Reaction(k_v1_r, [x2], [x1]),
-    Reaction(k_v2_f, [x2], [x3]),
-    Reaction(k_v2_r, [x3], [x2]),
-    Reaction(k_v3_f, [x3], [x4])
+    Reaction(k_v1_f*(x1 - x2/Keq_v1_f), [x1], [x2]),
+    Reaction(k_v1_r*(x2 - x1/Keq_v1_r), [x2], [x1]),
+    Reaction(k_v2_f*(x2 - x3/Keq_v2_f), [x2], [x3]),
+    Reaction(k_v2_r*(x3 - x2/Keq_v2_r), [x3], [x2]),
+    Reaction(k_v3_f * x3, [x3], [x4])
 ]
 @named rn = ReactionSystem(rxs ,t)
-println(rn.rxs)
+rn = complete(rn)
+for rx in rxs
+    println(rx)
+end
