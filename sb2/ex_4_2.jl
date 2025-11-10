6e617ca940d26d1b2836db30612c19c4d61f8612
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-11-10 12:21:23 -0700

    Issue #37
    Express reaction in the DSL.

--- sb2/ex_4_2.jl
+++ sb2/ex_4_2.jl
@@ -2,4 +2,19 @@ using Catalyst
 using CairoMakie
 using DifferentialEquations
 
-
+t = default_t()
+@species begin
+    x1(t)
+    x2(t)
+end
+@parameters begin
+    kf
+    kr
+end
+rn = @reaction_network ex_4_2 begin
+    kf*x1, x1 --> x2
+    kr*x2, x2 --> x1
+end
+for rx in reactions(rn)
+    println(rx)
+end
