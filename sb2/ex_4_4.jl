23d92d119ecaf1ee50291bb910c1327d3e1f2806
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-11-11 07:54:18 -0700

    Issue #37
    Hot burning mess of can't solve.

--- sb2/ex_4_4.jl
+++ sb2/ex_4_4.jl
@@ -3,6 +3,7 @@ using CairoMakie
 using DifferentialEquations
 
 @info "Making reaction network, parameters, and initial conditions..."
+
 rn = @reaction_network ex_4_4 begin
     @species begin
         x1(t)
@@ -23,12 +24,45 @@ rn = @reaction_network ex_4_4 begin
     k3*(x4 - x3/Keq_3), x3 --> x4
     k3*(x3 - x4/(1/Keq_3)), x4 --> x3
 end
+
 println("Reactions:")
 for rx in reactions(rn)
     println(rx)
 end
+
 println("ODEs:")
 osys = convert(ODESystem, rn)
 for eq in equations(osys)
     println(eq.rhs)
 end
+
+params = [:Keq_1 => 1.0, :k1 => 1.0, :k2 => 1.0, :Keq_3 => 1.0, :k3 => 1.0]
+println("Params ", params)
+u0 = [:x1 => 1.0, :x2 => 0.0, :x3 => 0.0, :x4 => 0.0]
+println("u0 ", u0)
+tspan = (0, 10)
+println("tspan ", tspan)
+
+@info "Making ODEProblem..."
+prob = ODEProblem(rn, u0, tspan, params)
+@info "Solving ODEs..."
+sol = solve(prob, Rodas5P(); reltol = 1.0e-8, abstol = 1.0e-10, maxiters = 1000)
+
+@info "Plotting..."
+sps = [:x1, :x2, :x3, :x4]
+size = (900, 600)
+fig = Figure(; size = size)
+ax = Axis(
+    fig[1, 1];
+    xlabel = "Time",
+    xscale = log10,
+    ylabel = "Concentration",
+    title = "Results",
+)
+for sp in sps
+    lines!(ax, sol.t, sol[sp, :]; label = string(sp), linewidth = 2)
+end
+axislegend(ax; position = :rb, framevisible = false)
+fig_filename = joinpath("sb2", "plots", "ex_4_4_sol_conc.png")
+save(fig_filename, fig)
+println("Wrote $fig_filename")
